d09b8d868bd4ae15c7365ac54f2a0cecbcb00463
skl-814 <85046404+skl-814@users.noreply.github.com>
2026-05-24 19:11:42 +0800

    fix bugs

--- app.py
+++ app.py
@@ -413,7 +413,7 @@ def status_info_page(status_code):
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True,port=2000)
+    app.run(debug=True,port=2000,host='0.0.0.0')
 
 # json.dump(statistics_j,statistics_f)
 # statistics_f.close()
